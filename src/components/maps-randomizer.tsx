594f7a83db657194a189b25d17d4bdf48540d3fb
Nafi Projo <projo27@gmail.com>
2025-12-08 02:11:50 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/components/maps-randomizer.tsx
+++ src/components/maps-randomizer.tsx
@@ -13,7 +13,7 @@ import {
 import { Button } from "@/components/ui/button";
 import { Map, Wand2, Satellite, PersonStanding } from "lucide-react";
 import { Skeleton } from "./ui/skeleton";
-import { getRandomPlace, ,  LatLng } from "@/app/actions/maps-randomizer-action";
+import { getRandomPlace, getTrulyRandomPlace,  LatLng } from "@/app/actions/maps-randomizer-action";
 import { Alert, AlertDescription, AlertTitle } from "./ui/alert";
 import { useRateLimiter } from "@/hooks/use-rate-limiter";
 import { useAuth } from "@/context/AuthContext";
